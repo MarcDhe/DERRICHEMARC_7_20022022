2cce027ce9135428cbd50b1d62e64edc53b312f0
MarcDhe <mderriche.pro@gmail.com>
2022-02-22 07:31:13 +0100

    ajout vue add router

---
+++ vue_frontend/src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <a :href="url"> {{ text }} </a>
+</template>
+
+<script>
+
+export default {
+ name: 'Login',
+ props: ['url', 'text']
+ 
+}
+
+</script>
+
+<style lang="scss">
+ 
+</style>
